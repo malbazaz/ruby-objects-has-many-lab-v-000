694acaadb797eebbe822826991678c4aa8140784
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-21 22:40:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,23 @@
 class Song 
   
-  attr_accessor :name, :artist 
+  attr_accessor :name, :artist, :all, :artist_name 
   
   def initialize(name)
     @name = name
     @@all = []
-    name = self.new(name)
-    @@all <<
+    @@all << name = self.new(name)
   end 
-
+  
+  def self.all
+    @@all 
+  end 
+  
+  def artist_name
+    if self.artist.name = nil 
+      nil 
+    else 
+      self.artist.name
+    end 
+  end 
 
 end 
